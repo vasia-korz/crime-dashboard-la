56bd71f8f4be420efafb871aee5e8ad90ed8d3a2
Andrew Chmutov <ulman.andre@gmail.com>
2024-06-04 15:50:27 +0200

    Read the outside of input, get areas

--- server.R
+++ server.R
@@ -2,43 +2,50 @@
 library(shiny)
 library(shinydashboard)
 library(DT)
+library(dplyr)
+
+dataset <- read.csv("data/dataset.csv")
+areas_df <- dataset %>%
+  select(AREA.NAME) %>%
+  na.omit %>%
+  group_by(AREA.NAME) %>%
+  summarize(count = n())
 
 # Define server logic
 shinyServer(function(input, output, session) {
-  dataset <- read.csv("data/dataset.csv")
-  
+
   # Reactive value to keep track of the table state
   table_state <- reactiveVal("full")
-  
+
   # Render the full data table with all columns
   output$full_table <- renderDataTable({
-    datatable(dataset, selection = 'single', options = list(pageLength = 10))
+    datatable(areas_df, selection = "single", options = list(pageLength = 10))
   })
-  
+
   # Render the short data table with only two columns
   output$short_table <- renderDataTable({
-    datatable(dataset[, c(1, 2)], selection = 'single', options = list(pageLength = 10)) # Assuming columns 1 and 2 are needed
+    datatable(areas_df[, c(1, 2)], selection = "single", options = list(pageLength = 10)) # Assuming columns 1 and 2 are needed
   })
-  
+
   # Observe the table selection
   observeEvent(input$full_table_rows_selected, {
     selected_row <- input$full_table_rows_selected
-    
+
     if (length(selected_row)) {
       table_state("short")
     }
   })
-  
+
   # Listen for custom message to reset the UI
   observeEvent(input$esc_key, {
     table_state("full")
   })
-  
+
   # Example plots
   output$plot1 <- renderPlot({ plot(cars) })
   output$plot2 <- renderPlot({ plot(pressure) })
   output$plot3 <- renderPlot({ plot(iris) })
-  
+
   # Output table state for conditional panels
   output$table_state <- reactive({
     table_state()
