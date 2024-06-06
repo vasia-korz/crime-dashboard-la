ae0685e6ded9d5b1410732e099cc760059905a18
Andrew Chmutov <ulman.andre@gmail.com>
2024-06-07 01:23:20 +0200

    Refactor UI

--- ui.R
+++ ui.R
@@ -5,20 +5,40 @@ library(DT)
 library(leaflet)
 library(plotly)
 
-vict_descent_choices <- c("All", "Hispanic", "White", "Black", "Asian", "Korean", "Filipino", "Chinese", "Japanese", "Vietnamese", "Native", "Indian", "Hawaiian", "Pacific", "Laotian", "Cambodian", "Guamanian", "Samoan")
+vict_descent_choices <- c(
+  "All",
+  "Hispanic",
+  "White",
+  "Black",
+  "Asian",
+  "Korean",
+  "Filipino",
+  "Chinese",
+  "Japanese",
+  "Vietnamese",
+  "Native",
+  "Indian",
+  "Hawaiian",
+  "Pacific",
+  "Laotian",
+  "Cambodian",
+  "Guamanian",
+  "Samoan"
+)
+
 vict_sex_choices <- c("All", "M", "F", "X", "H")
 
 # Define UI for application that displays a dashboard
 dashboardPage(
   dashboardHeader(title = "Superstore Sales Dashboard"),
-  
+
   dashboardSidebar(
     sidebarMenu(
       menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard"))
     ),
-    collapsed = T
+    collapsed = TRUE
   ),
-  
+
   dashboardBody(
     tags$head(
       tags$script(HTML(
@@ -38,41 +58,71 @@ dashboardPage(
         "
       ))
     ),
-    
+
     fluidRow(
-      box(width = 3, 
-          selectInput("vict.descent", "Victim Descent:",
-                      choices = vict_descent_choices),
+      box(width = 3,
+        selectInput(
+          "vict.descent",
+          "Victim Descent:",
+          choices = vict_descent_choices
+        ),
       ),
-      box(width = 3, 
-          selectInput("vict.sex", "Victim Sex:",
-                      choices = vict_sex_choices),
+      box(width = 3,
+        selectInput(
+          "vict.sex",
+          "Victim Sex:",
+          choices = vict_sex_choices
+        ),
       )
     ),
-    
+
     tabItems(
-      tabItem(tabName = "dashboard",
+      tabItem(
+        tabName = "dashboard",
+        fluidRow(
+          box(
+            title = "Crimes Solved vs. Not Solved",
+            status = "primary",
+            solidHeader = TRUE,
+            plotlyOutput("plot1"),
+            width = 6
+          ),
+          box(
+            title = "Number of Crimes by Victim Descent",
+            status = "primary",
+            solidHeader = TRUE,
+            plotlyOutput("plot2"),
+            width = 6
+          )
+        ),
+        fluidRow(
+          box(
+            title = "Top Customers",
+            status = "primary",
+            solidHeader = TRUE,
+            leafletOutput("crimemap", height = 500)
+          ),
+          box(
+            title = "Sales per Customer by Category",
+            status = "primary",
+            solidHeader = TRUE,
+            conditionalPanel(
+              condition = "output.table_state == 'full'",
+              dataTableOutput("full_table")
+            ),
+            conditionalPanel(
+              condition = "output.table_state == 'short'",
               fluidRow(
-                box(title = "Crimes Solved vs. Not Solved", status = "primary", solidHeader = TRUE, plotlyOutput("plot1"), width = 6),
-                box(title = "Number of Crimes by Victim Descent", status = "primary", solidHeader = TRUE, plotlyOutput("plot2"), width = 6)
-              ),
-              fluidRow(
-                box(title = "Top Customers", status = "primary", solidHeader = TRUE, leafletOutput("crimemap", height = 500)),
-                box(title = "Sales per Customer by Category", status = "primary", solidHeader = TRUE, 
-                    conditionalPanel(
-                      condition = "output.table_state == 'full'",
-                      dataTableOutput("full_table")
-                    ),
-                    conditionalPanel(
-                      condition = "output.table_state == 'short'",
-                      fluidRow(
-                        column(6, dataTableOutput("short_table")),
-                        column(6, div(id = "additional_content", "Additional content here"))
-                      )
-                    ), 
-                    width = 6
-                ),
+                column(6, dataTableOutput("short_table")),
+                column(6, div(
+                  id = "additional_content",
+                  "Additional content here"
+                ))
               )
+            ),
+            width = 6
+          ),
+        )
       )
     )
   )
